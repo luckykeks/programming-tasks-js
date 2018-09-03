f285e3955b6aa43689435050a699e82c7c4ef665
luckykeks <luckykeks@gmail.com>
2018-09-03 15:38:51 +0300

    function reduce was modified

--- reduce.js
+++ reduce.js
@@ -1,9 +1,14 @@
-function reduce(arr, condition, currentValue) {
-    let sum = currentValue;
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
+function reduce(array, callback, initialValue) {
+    if (!(array instanceof Array && typeof callback === 'function')) {
+        console.error('Incorrect function arguments');
+        return;
     }
-    return sum;
+    if (array.length === 0) {
+        return initialValue;
+    }
+    if (initialValue == undefined) initialValue = 0;
+    let currentValue = array.shift();
+    return reduce(array, callback, callback(initialValue, currentValue));
 }
 
-console.log(reduce([1, 2, 3, 4], (a, b) => a + b, 10));
+console.log(reduce([1, 2, 3, 4], (a, b) => a + b, 1));
